e7a03eebd7e84ccb2f12ddb769e76dd8db17d4d4
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-05-01 13:54:14 -0300

    Pequenos Ajustes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'; 
-import Routes from './router/Routes.jsx';
+import { createRoot } from 'react-dom/client';
+import Routes from './router/Routes.jsx'
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes />
+      <Routes.element />
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,29 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
-
 import App from "../App";
 import PaginaInicial from "../pages/PaginaInicial";
 import PaginaCadastroTrilha from "../pages/PaginaCadastro";
 import PaginaListaTrilhas from "../pages/PaginaListaTrilhas";
 
-const routes = createBrowserRouter([
- {
-  path: "/",
-  element: <App />,
+const Routes = createBrowserRouter({
   children: [
-   {
-    path: "/",
-    element: <PaginaInicial />
-   },
-   {
-    path: "/cadastro-trilha",
-    element: <PaginaCadastroTrilha />
-   },
-   {
-    path: "/lista-trilhas",
-    element: <PaginaListaTrilhas />
-   }
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <PaginaInicial />
+        },
+        {
+          path: "/cadastro-trilha",
+          element: <PaginaCadastroTrilha />
+        },
+        {
+          path: "/lista-trilhas",
+          element: <PaginaListaTrilhas />
+        }
+      ]
+    }
   ]
- }
-]);
+});
 
-export default routes;
+export default Routes;
+
